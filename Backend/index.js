667c4c724d90232c5a85003fd6c3b53545df1115
afuentes11 <afuentes11@cuc.edu.co>
2021-11-09 04:03:09 -0500

    test get http method

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,32 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 const db = require('./src/db/crud.js');
 
-db.getUsers();
+app.use(express.static('public'))
+
+
+app.get('/', function(req, res) {
+    res.send('mi primer servidor express');
+});
+
+//crea unanueva ruta
+app.get('/nueva-ruta', function(req, res) {
+    res.send('mi primera ruta en express')
+})
+
+app.get('/Users', function(req, res) {
+    res.json({
+        "user1": 'Andres',
+        'user2':'Fuentes'
+    });
+})
+
+app.get('/user/:id', function(req, res) {
+    const userID = req.params.id;
+    //consulto en la base de datos y trajo un usuario
+    res.send(userID +' andres fuentes');
+})
+app.listen(port);
+
 
