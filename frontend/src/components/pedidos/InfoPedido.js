66cf6264ecdb5cf462c81c34417f2343cc4dafaf
afuentes11 <afuentes11@cuc.edu.co>
2021-12-05 20:53:14 -0500

    Add props
    se añadio el codigo para mapear cada uno de los pedidos

--- frontend/src/components/pedidos/InfoPedido.js
+++ frontend/src/components/pedidos/InfoPedido.js
@@ -5,25 +5,28 @@ const Fila = (props) => {
     return (
         <>
             <Stack gap={3} direction = {'horizontal'} className='justify-content-between'>
-            <div >{props.titulo}:</div>
-            <div >{props.contenido}</div>
+            <div className='px-3' >{props.titulo}</div>
+            <div className='px-3'>{props.contenido}</div>
             </Stack>
         </>
     )
 }
 
-const InfoPedido = () => {
+const InfoPedido = (props) => {
     return (
         <>
-        <Card border="primary" style={{ width: '46rem', background: '#0a6ddb', color: 'white' }}>
-            <Stack gap={1} >
-                <Card.Header><h1 style={{alignSelf: 'center'}} >DETALLES DEL PEDIDO</h1></Card.Header>
-                <Fila titulo={'Fecha'} contenido={'Aca va la fecha a la que se solicitó el servicio'}/>
-                <Fila titulo={'Tipo servicio'} contenido={'Aca va el tipo de servicio'}/>
-                <Fila titulo={'Dirección recogida'} contenido={'contenido'}/>
-                <Fila titulo={'Dirección destino'} contenido={'contenido'}/> 
-                <Fila titulo={'Nombre de quien recibe'} contenido={'contenido'}/>
-                <Fila titulo={'Teléfono de quien recibe'} contenido={'contenido'}/>
+        <Card border="primary" style={{ minWidth:'530px' ,width: '50vw', background: '#0a6ddb', color: 'white' }}>
+            <Stack gap={3} >
+                <Card.Header>
+                    <h1 style={{ textAlign: 'center'}}> DETALLES DEL PEDIDO </h1>
+                </Card.Header>
+                <Fila titulo={'Fecha'} contenido={props.data.fecha}/>
+                <Fila titulo={'Tipo servicio'} contenido={props.data.tipoServicio}/>
+                <Fila titulo={'Dirección recogida'} contenido={props.data.direccionOrigen}/>
+                <Fila titulo={'Dirección destino'} contenido={props.data.direccionDestino}/> 
+                <Fila titulo={'Nombre de quien recibe'} contenido={props.data.nombreReceptor}/>
+                <Fila titulo={'Teléfono de quien recibe'} contenido={props.data.celularReceptor}/>
+                <Fila></Fila>
             </Stack>
         </Card>
         </>
