6b81b09cbb100ff1fa815b8c79e6754c2caacfc3
afuentes11 <afuentes11@cuc.edu.co>
2021-12-06 00:02:32 -0500

    Update URL

--- frontend/src/apis/UsersCRUD.js
+++ frontend/src/apis/UsersCRUD.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const URL= 'http://localhost:5000/users'; 
+const URL= 'https://serviexpress-backend-prueba.herokuapp.com/users'; 
 
 export function addUser(user,callback){
     axios.post(URL,user)
